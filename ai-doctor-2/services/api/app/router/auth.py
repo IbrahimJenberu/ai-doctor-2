8ffb62daf2e96441d8c77caa720d909f03134a51
Ibrahim Jenberu <152397228+IbrahimJenberu@users.noreply.github.com>
2025-02-23 07:02:01 -0800

    Update auth.py

--- ai-doctor-2/services/api/app/router/auth.py
+++ ai-doctor-2/services/api/app/router/auth.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from services.api.app.models.user import create_user, authenticate_user
+from services.api.app.schema.user import UserCreate, UserLogin
 
 router = APIRouter()
 
 @router.get("/auth")
 async def get_auth_data():
     return {"message": "Auth data"}
+
+@router.post("/register/")
+async def register_user(user: UserCreate):
+    return await create_user(user.username, user.password)
+
+@router.post("/login/")
+async def login_user(user: UserLogin):
+    return await authenticate_user(user.username, user.password)
