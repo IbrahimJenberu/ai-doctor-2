70bed4f5419c05800467e008ec7cb7b47cea6966
Ibrahim Jenberu <152397228+IbrahimJenberu@users.noreply.github.com>
2025-02-24 05:05:11 -0800

    Update main.py

--- ai-doctor-2/services/api/main.py
+++ ai-doctor-2/services/api/main.py
@@ -1,1 +1,19 @@
+# ADPPM/services/api/app/main.
+from fastapi import FastAPI
+from app.router import opd, card, lab, auth
 
+app = FastAPI()
+
+# Include the routers for different modules
+app.include_router(opd.router, prefix="/opd", tags=["OPD Room"])
+app.include_router(card.router, prefix="/card", tags=["Card Room"])
+app.include_router(lab.router, prefix="/lab", tags=["Lab Room"])
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to ADPPM API"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
