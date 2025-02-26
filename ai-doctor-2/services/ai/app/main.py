6a2829b1c198a2ec409d5e948967b3ab2af76f4a
Ibrahim Jenberu <152397228+IbrahimJenberu@users.noreply.github.com>
2025-02-25 23:34:52 -0800

    Update main.py

--- ai-doctor-2/services/ai/app/main.py
+++ ai-doctor-2/services/ai/app/main.py
@@ -9,3 +9,7 @@ app = FastAPI()
 app.include_router(xray.router, prefix="/xray", tags=["Chest X-ray Analysis"])
 app.include_router(mri.router, prefix="/mri", tags=["Brain MRI Analysis"])
 app.include_router(symptoms.router, prefix="/symptoms", tags=["Symptoms Analysis"])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8002)
